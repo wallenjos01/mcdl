58d77df7d4ff72d97f35b6e1111c3d8d172b81cd
wallenjos01 <wallenjos01@gmail.com>
2024-04-05 22:19:30 -0600

    MidnightProxy configuration

--- src/main/java/org/wallentines/mcdl/Installer.java
+++ src/main/java/org/wallentines/mcdl/Installer.java
@@ -10,7 +10,7 @@ public enum Installer {
     SPIGOT("spigot", new TaskList.Builder().then(SpigotUtils.BUILD_SPIGOT).then(VanillaUtil.FIRST_LAUNCH).then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     PAPER("paper", new TaskList.Builder().then(PaperUtils.DOWNLOAD_PAPER).then(VanillaUtil.FIRST_LAUNCH).then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     VELOCITY("velocity", new TaskList.Builder().then(PaperUtils.DOWNLOAD_VELOCITY).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
-    MIDNIGHT_PROXY("midnightproxy", new TaskList.Builder().then(MidnightProxyUtil.DOWNLOAD_MIDNIGHTPROXY).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
+    MIDNIGHT_PROXY("midnightproxy", new TaskList.Builder().then(MidnightProxyUtil.DOWNLOAD).then(MidnightProxyUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     MAVEN("maven", new TaskList.Builder().then(MavenUtil.DOWNLOAD_FABRIC).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     CUSTOM("custom", new TaskList.Builder().then(CustomUtil.COPY_CUSTOM_JAR).then(VanillaUtil.FIRST_LAUNCH).then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build());
 
--- src/main/java/org/wallentines/mcdl/util/MidnightProxyUtil.java
+++ src/main/java/org/wallentines/mcdl/util/MidnightProxyUtil.java
@@ -3,17 +3,20 @@ package org.wallentines.mcdl.util;
 import org.wallentines.mcdl.Task;
 import org.wallentines.mdcfg.ConfigObject;
 import org.wallentines.mdcfg.ConfigSection;
+import org.wallentines.mdcfg.codec.FileWrapper;
+import org.wallentines.mdcfg.codec.JSONCodec;
+import org.wallentines.mdcfg.serializer.ConfigContext;
 
 import java.io.File;
 
 public class MidnightProxyUtil {
 
     private static final String LATEST_API = "https://api.github.com/repos/wallenjos01/MidnightProxy/releases/latest";
-    private static final String ARTIFACT_URL = "https://api.github.com/repos/wallenjos01/MidnightProxy/releases/download/v%s/midnightproxy-proxy-%s.jar";
+    private static final String ARTIFACT_URL = "https://github.com/wallenjos01/MidnightProxy/releases/download/v%s/midnightproxy-proxy-%s.jar";
 
     public static Task.Result downloadMidnightProxy(String version, File output) {
         if(!DownloadUtil.downloadBytes(ARTIFACT_URL.formatted(version, version), output)) {
-            return Task.Result.error("Unable to download Maven artifact!");
+            return Task.Result.error("Unable to download MidnightProxy!");
         }
         return Task.Result.success();
     }
@@ -28,7 +31,7 @@ public class MidnightProxyUtil {
         return null;
     }
 
-    public static final Task DOWNLOAD_MIDNIGHTPROXY = queue -> {
+    public static final Task DOWNLOAD = queue -> {
 
         ConfigSection config = queue.getConfig();
 
@@ -42,6 +45,31 @@ public class MidnightProxyUtil {
         queue.getLogger().info(String.format("Downloading MidnightProxy version %s...", serverVersion));
         Task.Result dlRes = downloadMidnightProxy(serverVersion, serverJar);
         if(dlRes.isError()) return dlRes;
+
+        return Task.Result.success();
+    };
+
+    public static final Task CONFIGURE = queue -> {
+
+        ConfigSection config = queue.getConfig();
+        ConfigSection out = new ConfigSection();
+
+        // Port
+        if (config.has("port")) {
+            try {
+                int port = Integer.parseInt(config.getString("port"));
+                if(port < 1 || port > Short.MAX_VALUE) {
+                    return Task.Result.error("Server port out of range [1,65535]!");
+                }
+                out.set("port", port);
+            } catch (NumberFormatException ex) {
+                return Task.Result.error("Unable to parse port number!");
+            }
+        }
+
+        FileWrapper<ConfigObject> wrapper = new FileWrapper<>(ConfigContext.INSTANCE, JSONCodec.fileCodec(), new File(FileUtil.getWorkingDir(queue.getConfig()), "config.json"));
+        wrapper.setRoot(out);
+        wrapper.save();
 
         return Task.Result.success();
     };
