03a7f050b2a3d715d73ceb99825e33c46bcc9e40
wallenjos01 <wallenjos01@gmail.com>
2024-10-21 20:14:19 -0600

    Add "none" type, add .gitattributes

--- src/main/java/org/wallentines/mcdl/Installer.java
+++ src/main/java/org/wallentines/mcdl/Installer.java
@@ -12,10 +12,11 @@ public enum Installer {
     VELOCITY("velocity", new TaskList.Builder().then(PaperUtils.DOWNLOAD_VELOCITY).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     MIDNIGHT_PROXY("midnightproxy", new TaskList.Builder().then(MidnightProxyUtil.DOWNLOAD).then(MidnightProxyUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
     MAVEN("maven", new TaskList.Builder().then(MavenUtil.DOWNLOAD_MAVEN).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
-    CUSTOM("custom", new TaskList.Builder().then(CustomUtil.COPY_CUSTOM_JAR).then(VanillaUtil.FIRST_LAUNCH).then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build());
+    CUSTOM("custom", new TaskList.Builder().then(CustomUtil.COPY_CUSTOM_JAR).then(VanillaUtil.FIRST_LAUNCH).then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),
+    NONE("none", new TaskList.Builder().then(VanillaUtil.CONFIGURE).then(FileUtil.GENERATE_LAUNCH_SCRIPTS).build()),;
 
     private final TaskList prefab;
-    private final String id;
+    public final String id;
 
     Installer(String id, TaskList prefab) {
         this.id = id;
@@ -25,10 +26,6 @@ public enum Installer {
     public void run(ConfigSection config) {
         TaskQueue queue = prefab.createInstance(config);
         queue.run();
-    }
-
-    public String getId() {
-        return id;
     }
 
     public static Installer byId(String id) {
